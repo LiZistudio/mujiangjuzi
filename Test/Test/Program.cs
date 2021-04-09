0017c57de83d5ded728375c9e89128456f39bd51
LiZi <mujiangjuzi@foxmail.com>
2021-04-09 17:23:27 +0800

    C#初学
    C#初学

---
+++ Test/Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fun num = new Fun();
+            num.Add(4, 8);
+        }
+    }
+    class Fun
+    {
+        public void Add(int a,int b)
+        {
+            Console.WriteLine(a + b);
+        }
+    }
+}
