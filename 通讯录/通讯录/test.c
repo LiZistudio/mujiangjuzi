ff7b0e0f0b34d0f572be0bf8d2b8d27b6e540fd9
LiZi <mujiangjuzi@foxmail.com>
2021-04-19 12:12:01 +0800

    通讯录v01.4.19
     通讯录v01.4.19

--- 通讯录/通讯录/test.c
+++ 通讯录/通讯录/test.c
@@ -14,8 +14,51 @@
 
 #include "tongxun.h"
 
+void menu()
+{
+	printf("**********************************\n");
+	printf("****1.增加*********2.删除**********\n");
+	printf("****3.查询*********4.修改**********\n");
+	printf("****5.显示*********6.排序**********\n");
+	printf("****0.退出*********7.扩展**********\n");
+	printf("**********************************\n");
+}
 int main()
 {
-
+	int input = 0;
+	//创建通讯录
+	struct Contact con;//con就是通讯录，里面包含1000个元素的数组和size
+	//初始化通讯录
+	InitContact(&con);
+	//
+	do
+	{
+		menu();
+		printf("请选择>:");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			AddContact(&con);
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			ShwoContact(&con);
+			break;
+		case 6:
+			break;
+		case 0:
+			printf("退出通讯录\n");
+			break;
+		default:
+			printf("选择错误!\n");
+			break;
+		}
+	} while (input);
 	return 0;
 }
--- 通讯录/通讯录/tongxun.h
+++ 通讯录/通讯录/tongxun.h
@@ -1,5 +1,30 @@
 #pragma once
 
+#define _CRT_SECURE_NO_WARNINGS 1
+
 #include <stdio.h>
 #include <string.h>
 
+#define MAX 1000
+#define MAX_name 20
+#define MAX_sex 5
+#define MAX_tele 12
+#define MAX_addr 30
+struct PeoInfo
+{
+	char name[MAX_name];
+	int age;
+	char sex[MAX_sex];
+	char tele[MAX_tele];
+	char addr[MAX_addr];
+};
+//通讯录类型
+struct Contact
+{
+	struct PeoInfo data[MAX];//存放1000个信息
+	int size;//记录当前已经有的元素个数
+};
+//声明函数
+void InitContact(struct Contact* ps);
+void AddContact(struct Contact* ps);
+void ShwoContact(const struct Contact* ps);
