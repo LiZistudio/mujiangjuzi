f612b22769d9322ce6cd0c9439f24e4c04dc7961
LiShendu <mujiangjuzi@foxmail.com>
2021-08-03 21:28:34 +0800

    Add files via upload

---
+++ styledemo/main.cpp
@@ -0,0 +1,33 @@
+﻿#pragma execution_character_set("utf-8")
+#include "frmmain.h"
+#include <QApplication>
+#include <QTextCodec>
+
+int main(int argc, char *argv[])
+{
+#if (QT_VERSION >= QT_VERSION_CHECK(6,0,0))
+    QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::Floor);
+#endif
+    QApplication a(argc, argv);
+    a.setFont(QFont("Microsoft Yahei", 9));
+
+#if (QT_VERSION <= QT_VERSION_CHECK(5,0,0))
+#if _MSC_VER
+    QTextCodec *codec = QTextCodec::codecForName("gbk");
+#else
+    QTextCodec *codec = QTextCodec::codecForName("utf-8");
+#endif
+    QTextCodec::setCodecForLocale(codec);
+    QTextCodec::setCodecForCStrings(codec);
+    QTextCodec::setCodecForTr(codec);
+#else
+    QTextCodec *codec = QTextCodec::codecForName("utf-8");
+    QTextCodec::setCodecForLocale(codec);
+#endif
+
+    frmMain w;
+    w.setWindowTitle("样式表示例 (QQ: 517216493 WX: feiyangqingyun)");
+    w.show();    
+
+    return a.exec();
+}
