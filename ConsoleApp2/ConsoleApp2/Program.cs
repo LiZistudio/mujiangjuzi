a26801c259ca7ae2384fc14cc3af4cd0c39a312e
LiShendu <mujiangjuzi@foxmail.com>
2021-09-08 21:44:43 +0800

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using ConsoleApp2;
+using ConsloeApp3;
+
+namespace ConsoleApp2
+{
+    class program
+    {
+        public static void JiujiuChengfabiao()
+        {
+            Console.WriteLine("你好 栗子\n");
+            int i = 0;
+            int j = 0;
+            for (i=1;i<=9;i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("{0}X{1}={2}\t", i,j,i * j);
+                }
+            Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+    class Shixian
+    {
+        public static void Main()
+        {
+            program.JiujiuChengfabiao();
+            program2.Print();
+        }
+    }
+}
+namespace ConsloeApp3
+{
+    class program2
+    {
+        public static void Print()
+        {
+            Console.WriteLine("\ni love you although you don`t love me");
+        }
+    }
+}
